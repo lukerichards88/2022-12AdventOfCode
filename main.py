7f797f495ac3ba7738081684f0e1dd69c63f134e
Luke Richards <worthingtonrichards@gmail.com>
2022-12-04 22:01:46 +0000

    Task Not Started

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for x in range(5, 25):
+    day = f"Day{x}"
+    os.mkdir(day)
+    for y in range(1, 3):
+        task = f"Task{y}"
+        filename = f"{day}/{day}{task}.py"
+        with open(filename, 'w') as file:
+            file.write("\n")
