ddf0b09cb25c25f01f3ff3a24754ae821d5992ac
root <root@instance-1.us-central1-a.c.rprime-search.internal>
2022-12-06 20:55:40 +0000

    Day 6 Complete

--- Day6/Day6Task2.py
+++ Day6/Day6Task2.py
@@ -1,1 +1,15 @@
+def getMarkerPos(stream: str, streamLength: int):
+    end = len(stream)
+    start = streamLength
+    for position in range(start, end):
+        marker = stream[position - streamLength: position]
+        marker = set(marker)
+        if len(marker) == streamLength:
+            return position
 
+filename = 'Day6Input.txt'
+#filename = 'test.txt'
+with open(filename) as file:
+    for line in file:
+        line = line.replace('\n','')
+        print(getMarkerPos(line, 14))
