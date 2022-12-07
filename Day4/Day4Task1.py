d8f27ca3a72ed1de7814641d9265086a93a2e08d
root <root@instance-1.us-central1-a.c.rprime-search.internal>
2022-12-07 12:30:14 +0000

    Renamed folder name

--- Day4/Day4Task1.py
+++
@@ -1,8 +0,0 @@
-def checkContained(pair):
-    elfOne, elfTwo = [(int(elf[0]), int(elf[1])) for elf in pair]
-    return (elfOne[0] <= elfTwo[0] and elfOne[1] >= elfTwo[1]) or (elfOne[0] >= elfTwo[0] and elfOne[1] <= elfTwo[1])
-contained = 0
-allocations = [[pair[0].split('-'), pair[1].split('-')] for pair in [line.replace('\n','').split(',') for line in open('Day4Input.txt')]]
-for pair in allocations:
-    contained += checkContained(pair)
-print(contained)
