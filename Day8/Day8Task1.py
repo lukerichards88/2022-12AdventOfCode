5c23ca550d54b55b71cb17f83cf646c40069b3ff
root <root@instance-1.us-central1-a.c.rprime-search.internal>
2022-12-08 19:43:20 +0000

    Complete

--- Day8/Day8Task1.py
+++ Day8/Day8Task1.py
@@ -1,1 +1,63 @@
+width = 0
+length = 0
+array = ""
+filename = 'Day8Input.txt'
 
+def Row(array, position, width):
+  rowNum = position // width
+  rowStart = rowNum * width
+  rowEnd = rowStart + width
+  return array[rowStart:rowEnd]
+
+def RowSplit(array, position, width):
+  row = Row(array, position, width)
+  positionInRow = position % width
+  splitLow, splitHigh = row[:positionInRow], row[positionInRow + 1:]
+  return splitLow, splitHigh
+
+def Column(array, position, width):
+  columnNum = position % width
+  return array[columnNum::width]
+  
+def ColumnSplit(array, position, width):
+  column = Column(array, position, width)
+  row = position // width
+  splitLow = column[:row]
+  splitHigh = column[row + 1:]
+  return splitLow, splitHigh
+  
+def checkVisibleOneD(treeHeight, split1, split2):
+  if 0 in (len(split1), len(split2)):
+    return True
+  maxOne = max(split1)
+  maxTwo = max(split2)
+  if treeHeight > maxOne or treeHeight > maxTwo:
+    return True
+  return False
+  
+
+def checkVisible(array, position, width):
+  treeHeight = array[position]
+  rowBefore, rowAfter = RowSplit(array, position, width)
+  if checkVisibleOneD(treeHeight, rowBefore, rowAfter):
+    return True
+  if checkVisibleOneD(treeHeight, *ColumnSplit(array, position, width)):
+    return True
+  return False
+
+
+with open(filename) as file:
+  for line in file:
+    line = line.replace('\n','')
+    if not width:
+      width = len(line)
+    length += 1
+    array += line
+    
+if __name__ == "__main__":  
+  visibleCount = 0
+  for position in range(len(array)):
+    if checkVisible(array, position, width):
+      visibleCount += 1
+  print()
+  print(visibleCount)
--- Day8/Day8Task2.py
+++ Day8/Day8Task2.py
@@ -1,1 +1,40 @@
+from Day8Task1 import *
+filename = 'Day8Test.txt'
+def checkVisibility(treeHeight, directionTuple, verbose=False):
+  if len(directionTuple) == 0:
+    return 0
+  if max((int(x) for x in directionTuple)) < treeHeight:
+    if verbose: print("full visibility", len(directionTuple))
+    return len(directionTuple) 
+  for distance, tree in enumerate(directionTuple, start=1):
+    if int(tree) >= treeHeight:
+      if verbose: print(directionTuple, tree, "less visibility", distance)
+      return distance
+
+def getScenicScore(treeHeight, directionTuples, verbose=False):
+  scenicScore = 1
+  for directionTuple in directionTuples:
+    if verbose: print(directionTuple)
+    visibility = checkVisibility(treeHeight, directionTuple, verbose)
+    if visibility:
+      scenicScore *= visibility
+    else:
+      return 0
+  return scenicScore
+
+def evaluate(array, position, width, verbose=False):
+  rowLow, rowHigh = RowSplit(array, position, width)
+  colLow, colHigh = ColumnSplit(array, position, width)
+  rowLow, colLow = (x[::-1] for x in (rowLow, colLow))
+  treeHeight = int(array[position])
+  return getScenicScore(treeHeight, (rowLow, rowHigh, colLow, colHigh), verbose)
+
+bestFound = 0
+for treePosition in range(len(array)):
+  score = evaluate(array, treePosition, width)
+  if score > bestFound:
+    bestFound = score
+    print(treePosition, width, treePosition % width, treePosition // width, score, *RowSplit(array, treePosition, width))
+    #evaluate(array, treePosition, width, True)
+print(bestFound)
 
