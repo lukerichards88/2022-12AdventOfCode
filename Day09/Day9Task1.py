f93174d5d4e7a34ddab15d09ae7f14393875b6fa
Luke Richards <worthingtonrichards@gmail.com>
2022-12-09 22:25:01 +0000

    Complete

--- Day09/Day9Task1.py
+++ Day09/Day9Task1.py
@@ -1,137 +1,104 @@
-xDirections = {'L': -1, 'R': 1}
-yDirections = {'U': 1, 'D': -1}
-import itertools
-def XNOR(a, b):
-    return not(a ^ b)
+def euclideanDistance(*args):
+    total = 0
+    for arg in args:
+        a, b = arg
+        d = a - b
+        total += d ** 2
+    return total ** 0.5
 
-def euclideanDistance(dX, dY):
-    return (dX ** 2 + dY ** 2) ** 0.5
+
 class Knot:
+    def __init__(self, hasFollower=1):
+        self.X = 0
+        self.Y = 0
+        self.numberOfFollowers = hasFollower
+        self.visited = {(0, 0)}
 
-    def __init__(self, x, y, name="H", hasFollower=True, followerx=None, followery=None):
-        self.xPos = x
-        self.yPos = y
-        self.name = name
         if hasFollower:
-            if followerx is None:
-                followerx = x
-                followery = y
-            self.follower = Knot(followerx, followery, name="T", hasFollower=False)
+            self.F = Knot(hasFollower=hasFollower - 1)
         else:
-            self.followerx = self.followery = self.follower = None
-        self.visited = {(self.xPos, self.yPos)}
+            self.F = None
 
-    def move(self, direction):
-        self.xPos += xDirections.get(direction, 0)
-        self.yPos += yDirections.get(direction, 0)
-        if self.follower is not None:
-            self.moveFollowerNoArg()
+    def moveAbsolute(self, newPos):
+        newX, newY = newPos
+        self.X = newX
+        self.Y = newY
+        self.visited.add((newX, newY))
 
-    def moveFollowerNoArg(self):
-        selfX, selfY = self.curLoc()
-        follX, follY = self.follower.curLoc()
-        dX, dY = (selfX - follX), (selfY - follY)
-        distance = euclideanDistance(dX, dY)
-        if distance <= 2 ** 0.5:
+    def curPos(self):
+        return self.X, self.Y
+
+    def move(self, vector):
+        dX, dY = vector
+        self.X += dX
+        self.Y += dY
+        self.visited.add((self.X, self.Y))
+        if self.F:
+            self.moveFollower()
+
+    def moveFollower(self):
+        sX, sY = self.curPos()
+        fX, fY = self.F.curPos()
+        if (sX, sY) == (fX, fY):
             return
-        else:
-            directions = (-1,0,1)
-            vectors = set(itertools.permutations(directions, 2))
-            mindistance = 100
-            for vector in vectors:
-                vX, vY = vector
-                newFX = self.xPos + vX
-                newFY = self.yPos + vY
-                fDX = follX - newFX
-                fDY = follY - newFY
-                distance = euclideanDistance(fDX, fDY)
-                if distance < mindistance:
-                    mindistance = distance
-                    candidateX, candidateY = newFX, newFY
-            self.follower.moveAbsolute(candidateX, candidateY)
+        if euclideanDistance((sX, fX), (sY, fY)) in (1, 2 ** 0.5):
+            return
+        if sX == fX and abs(sY - fY) == 2:
+            newY = (sY + fY) / 2
+            newX = fX
+            fVector = (newX - fX, newY - fY)
+            self.F.move(fVector)
+            return
+        if sY == fY and abs(sX - fX) == 2:
+            newX = (sX + fX) / 2
+            newY = fY
+            fVector = (newX - fX, newY - fY)
+            self.F.move(fVector)
+            return
+        if euclideanDistance((sX, fX), (sY, fY)) == 5 ** 0.5:
+            if abs(sY - fY) == 2:
+                newY = (sY + fY) / 2
+                newX = sX
+                fVector = (newX - fX, newY - fY)
+                self.F.move(fVector)
+                return
+            if abs(sX - fX) == 2:
+                newX = (sX + fX) / 2
+                newY = sY
+                fVector = (newX - fX, newY - fY)
+                self.F.move(fVector)
+                return
+        if euclideanDistance((sX, fX), (sY, fY)) == 8 ** 0.5:
+            newX = (sX + fX) / 2
+            newY = (sY + fY) / 2
+            fVector = (newX - fX, newY - fY)
+            self.F.move(fVector)
+            return
+        print(sX, sY, fX, fY)
+        raise ValueError('WTF')
 
 
-    def moveAbsolute(self, x, y):
-        self.xPos, self.yPos = x, y
-        self.visited.add((x, y))
+vectors = {
+    'U': (0, 1),
+    'D': (0, -1),
+    'L': (-1, 0),
+    'R': (1, 0)
+}
 
-    def curLoc(self):
-        return (self.xPos, self.yPos)
-
-    def followerMoveVector(self, thisDirection: str):
-        this, follower = (self.curLoc(), self.follower.curLoc())
-        thisX, thisY = this
-        follX, follY = follower
-        if this == follower:
-            return (0,0)
-        if thisDirection in ('U', 'D'):
-            if thisX == follX:
-                thisIsAbove = (thisY > follY)
-                moveUp = (thisDirection == 'U')
-                move = XNOR(thisIsAbove, moveUp)
-                if not move:
-                    return (0,0)
-                return (0,yDirections[thisDirection])
-            else:
-                if thisY == follY:
-                    return (0,0)
-                xMove = thisX - follX
-                yMove = yDirections[thisDirection]
-                return (xMove, yMove)
-        else:
-            if thisY == follY:
-                thisIsRight = (thisX > follX)
-                moveRight = (thisDirection == 'R')
-                move = XNOR(thisIsRight, moveRight)
-                if not move:
-                    return (0,0)
-                return (xDirections[thisDirection], 0)
-            else:
-                if thisX == follX:
-                    return (0,0)
-                yMove = thisY - follY
-                xMove = xDirections[thisDirection]
-                return (xMove, yMove)
-
-    def moveString(self, line: str):
-        direction, distance = line.replace('\n','').split(' ')
-        distance = int(distance)
-        for movement in range(distance):
-            self.moveFollower(direction)
-            self.xPos += xDirections.get(direction, 0)
-            self.yPos += yDirections.get(direction, 0)
-            #print("Current Move:",direction)
-            #print("H pos", self.curLoc())
-            #print("T pos", self.follower.curLoc())
-            xA, yA = self.curLoc()
-            xB, yB = self.follower.curLoc()
-            dX = xA - xB
-            dY = yA - yB
-            eucLideanDistance = (dX ** 2 + dY ** 2) ** 0.5
-            closeEnough = (eucLideanDistance < ((2 ** 0.5) + 0.001))
-            self.visited.add((self.xPos, self.yPos))
-            #print(eucLideanDistance, closeEnough)
-            #approved = input("Check: ") == ""
-            if not closeEnough:
-                print("ERROR")
-                break
-
-    def moveVector(self, direction: tuple):
-        xMove, yMove = direction
-        self.xPos += xMove
-        self.yPos += yMove
-        self.visited.add((self.xPos, self.yPos))
-
-    def moveFollower(self, directionString):
-        vector = self.followerMoveVector(directionString)
-        self.follower.moveVector(vector)
-
-H = Knot(0,0)
-filename = 'Day9Input.txt'
+H = Knot(hasFollower=9)
+# filename = "Day9Test.txt"
+filename = "Day9Input.txt"
 with open(filename) as file:
     for line in file:
-        direction, number = line.split(' ')
-        for move in range(int(number)):
-            H.move(direction)
-
-print(len(H.follower.visited))
+        direction, distance = [line.split()[0], int(line.split()[1])]
+        for m in range(distance):
+            H.move(vectors[direction])
+F = H.F
+visited = F.visited
+print("Task 1")
+print(len(visited))
+print("Task 2")
+for x in range(9):
+    visited = len(F.visited)
+    F = F.F
+print(visited)
