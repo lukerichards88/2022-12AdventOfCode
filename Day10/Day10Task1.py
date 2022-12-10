c738e4a52cfa29322166a1a6bf53cf2f4349eb3f
Luke Richards <worthingtonrichards@gmail.com>
2022-12-10 22:04:28 +0000

    Complete

--- Day10/Day10Task1.py
+++ Day10/Day10Task1.py
@@ -1,5 +1,7 @@
 filename = 'Day10Input.txt'
 actions = (None, 'noop', 'addx')
+
+
 class Action:
     def __init__(self, instruction=None, value=0):
         self.remainingClicks = actions.index(instruction)
@@ -8,13 +10,17 @@ class Action:
     def click(self):
         self.remainingClicks -= 1
         return self.remainingClicks
+
+
 class Clock:
     def __init__(self):
         self.clock = 1
         self.X = 1
         self.Action = Action()
+
     def spritePos(self):
-        return (self.X + x - 1 for x in range(3))
+        return (self.X + r - 1 for r in range(3))
+
     def start(self, instruction=None):
         if not instruction:
             pass
@@ -35,6 +41,7 @@ class Clock:
     def strength(self):
         return self.clock * self.X
 
+
 class Manager:
     def __init__(self):
         self.Clock = Clock()
@@ -42,20 +49,21 @@ class Manager:
         self.CRT = ""
 
     def receiveInstruction(self, instruction=None):
-        instruction = instruction.replace('\n','')
+        instruction = instruction.replace('\n', '')
         self.start(instruction)
         while self.Clock.Action.remainingClicks > 0:
-            self.CRT += (' ','#')[(self.Clock.clock - 1) % 40 in self.Clock.spritePos()]
+            self.CRT += (' ', '#')[(self.Clock.clock - 1) % 40 in self.Clock.spritePos()]
             self.end()
             self.start()
 
-
     def start(self, instruction=None):
         self.Clock.start(instruction)
+
     def end(self):
         if self.Clock.clock % 40 == 20:
             self.strengths.append(self.Clock.strength())
         self.Clock.end()
+
 
 with open(filename) as file:
     line = file.readline()
@@ -68,5 +76,5 @@ with open(filename) as file:
 S = Manager.strengths
 print(sum(S))
 
-for x in range(len(Manager.CRT)//40):
-    print(Manager.CRT[40 * x:40*(x + 1)])
+for x in range(len(Manager.CRT) // 40):
+    print(Manager.CRT[40 * x:40 * (x + 1)])
