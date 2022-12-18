cd75020056c72a0a1c971d1ad8149b3ee335d13e
Luke Richards <worthingtonrichards@gmail.com>
2022-12-18 22:21:44 +0000

    Complete

--- Day18/Day18Task1.py
+++ Day18/Day18Task1.py
@@ -1,9 +1,72 @@
-filename = 'Day18Test.txt'
-# filename = 'Day18Input.txt'
+# filename = 'Day18TestTiny.txt'
+# filename = 'Day18Test.txt'
+filename = 'Day18Input.txt'
+
+
+# xyz
+# yzx
+# zxy
+
+# So...
+# xy, yz, zx
+def parse(fName: str, verbose: bool = False) -> tuple[tuple[int, ...]]:
+    with open(fName, 'r') as file:
+        cubes = set()
+        for line in file:
+            cube = line.strip().split(',')
+            if verbose:
+                print(cube)
+            cubes.add(tuple(int(c) for c in cube))
+    return tuple(cubes)
+
+
+def indexCubes(cubes: tuple[tuple[int, ...]]) -> tuple[dict, ...]:
+    xy = {}
+    yz = {}
+    xz = {}
+    for cube in cubes:
+        x, y, z = cube
+        xy.setdefault(x, {}).setdefault(y, set()).add(z)
+        yz.setdefault(y, {}).setdefault(z, set()).add(x)
+        xz.setdefault(x, {}).setdefault(z, set()).add(y)
+
+    return xy, yz, xz
+
+
+def CheckForNeighbours(cube: tuple[int, ...], indexes: tuple[dict, ...]) -> int:
+    x, y, z = cube
+    xy, yz, xz = indexes
+    neighbourCount = 0
+    for zN in (z + 1, z - 1):
+        try:
+            neighbourCount += zN in xy[x][y]
+        except KeyError:
+            pass
+    for xN in (x + 1, x - 1):
+        try:
+            neighbourCount += xN in yz[y][z]
+        except KeyError:
+            pass
+    for yN in (y + 1, y - 1):
+        try:
+            neighbourCount += yN in xz[x][z]
+        except KeyError:
+            pass
+    return neighbourCount
+
+
+def FindTotalNeighbours(cubes: tuple[tuple[int, ...], ...], indexes: tuple[dict, ...]) -> int:
+    TotalNeighbours = 0
+    for cube in cubes:
+        TotalNeighbours += CheckForNeighbours(cube, indexes)
+    return TotalNeighbours
+
 
 def main():
-    # main function goes here
-    pass
+    cubes = parse(filename)
+    totalCubes = len(cubes)
+    indexes = indexCubes(cubes)
+    print(6 * totalCubes - FindTotalNeighbours(cubes, indexes))
 
 
 if __name__ == "__main__":
--- Day18/Day18Task2.py
+++ Day18/Day18Task2.py
@@ -1,11 +1,115 @@
-filename = 'Day18Test.txt'
-# filename = 'Day18Input.txt'
+
 from Day18Task1 import *
+filename = 'Day18Test.txt'
+filename = 'Day18Input.txt'
+
+class Cube:
+    def __init__(self, x: int, y: int, z: int, substance: str = 'air', status: 'str'=None):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.position = (x, y, z)
+        self.substance = substance
+        self.status = status
+
+
+class SystemManager:
+    def __init__(self, indexes):
+        (
+            (self.xMin, self.xMax),
+            (self.yMin, self.yMax),
+            (self.zMin, self.zMax)
+        ) = self.findBoundary(indexes)
+        self.indexes = indexes
+        self.boundary = self.findBoundary(indexes)
+        self.cubes = {}
+        for x in range(self.xMin, self.xMax + 1):
+            for y in range(self.yMin, self.yMax + 1):
+                for z in range(self.zMin, self.zMax + 1):
+                    if x in self.boundary[0] or y in self.boundary[1] or z in self.boundary[2]:
+                        self.cubes.setdefault(x, {}).setdefault(y, {}).setdefault(z, Cube(x, y, z, 'air', 'outside'))
+                    self.cubes.setdefault(x, {}).setdefault(y, {}).setdefault(z, Cube(x, y, z, 'air'))
+
+    def findBoundary(self, indexes):
+        xy, yz, xz = indexes
+        minX = min(*xy.keys())
+        maxX = max(*xy.keys())
+        minY = min(*yz.keys())
+        maxY = max(*yz.keys())
+        yDicts = tuple(xy.values())
+        zValues = set()
+        for yDict in yDicts:
+            zValues.update(*yDict.values())
+        minZ = min(zValues)
+        maxZ = max(zValues)
+        return (
+            (minX-1, maxX+1),
+            (minY-1, maxY+1),
+            (minZ-1, maxZ+1)
+        )
+
+    def addCube(self, x, y, z, substance, neighbours):
+        status = (None, 'encased')[neighbours == 6]
+        self.cubes[x][y][z] = Cube(x, y, z, substance, status)
+    def findCube(self, x, y, z):
+        return self.cubes[x][y][z]
+
+    def airNeighbours(self, x, y, z):
+        xOptions = []
+        yOptions = []
+        zOptions = []
+        cube = x, y, z
+        if x - 1 >= self.xMin: xOptions.append(x - 1)
+        if x + 1 <= self.xMax: xOptions.append(x + 1)
+        if y - 1 >= self.yMin: yOptions.append(y - 1)
+        if y + 1 <= self.yMax: yOptions.append(y + 1)
+        if z - 1 >= self.zMin: zOptions.append(z - 1)
+        if z + 1 <= self.zMax: zOptions.append(z + 1)
+        options = []
+        for xOpt in xOptions:
+            options.append((xOpt, y, z))
+        for yOpt in yOptions:
+            options.append((x,yOpt, z))
+        for zOpt in zOptions:
+            options.append((x, y, zOpt))
+        outputOptions = []
+        for option in options:
+            if self.findCube(*option).substance == 'air' and self.findCube(*option).status != 'addedtoqueue':
+                outputOptions.append(option)
+        return outputOptions
+        
+
+    def breadthSearch(self):
+        totalContactFaces = 0
+        (x, _), (y, _), (z, _) = self.boundary
+        queue = self.airNeighbours(x, y, z)
+        nextCube = x, y, z
+        while len(queue) > 0:
+            for cube in self.airNeighbours(*nextCube):
+                if self.findCube(*cube).status != "addedtoqueue":
+                    queue.append(cube)
+                    self.findCube(*cube).status = "addedtoqueue"
+            totalContactFaces += CheckForNeighbours(nextCube, self.indexes)
+
+            nextCube = queue.pop(0)
+        return totalContactFaces
 
 def main():
-    # main function goes here
-    pass
-
+    cubes = parse(filename)
+    indexes = indexCubes(cubes)
+    xy, yz, xz = indexes
+    yDicts = tuple(xy.values())
+    zValues = set()
+    for yDict in yDicts:
+        zValues.update(*yDict.values())
+    system = SystemManager(indexes)
+    for cube in cubes:
+        system.addCube(*cube, 'lava', CheckForNeighbours(cube, indexes))
+    print(system.breadthSearch())
 
 if __name__ == "__main__":
+    from timeit import timeit
     main()
+    setup = "from Day18Task1 import parse, CheckForNeighbours, indexCubes; from __main__ import main; filename='Day18Input.txt'"
+    time = timeit('main()', setup=setup, number=1000)
+    print("Average time per iteration:", time, "ms.")
